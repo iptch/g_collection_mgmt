0c1f7760a2a264b7faa433e4f23b52656825bb35
Vinu <kevinduss1@gmail.com>
2024-02-25 15:57:32 +0000

    added script to copy all images in bucket and name them based on email

---
+++ image_copy/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import sqlalchemy
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, ContainerClient
+
+
+def copy_blob(container_client: ContainerClient, source_blob_name: str, target_blob_name: str):
+    source_blob_url = f"https://gcollection.blob.core.windows.net/card-originals/{source_blob_name}"
+    copied_blob = container_client.get_blob_client(target_blob_name)
+    print(f'Copying blob from {source_blob_name} to {target_blob_name}')
+    copied_blob.start_copy_from_url(source_blob_url)
+
+
+def main():
+    # Connect to Azure
+    credential = DefaultAzureCredential()
+    # Connect to Azure Blob Storage
+    blob_service_client = BlobServiceClient(account_url="https://gcollection.blob.core.windows.net", credential=credential)
+    container_client = blob_service_client.get_container_client("card-originals")
+
+    # Connect to local DB
+    engine = sqlalchemy.create_engine('sqlite:///db.sqlite3')
+    df = pd.read_sql('SELECT email, acronym FROM core_card', con=engine)
+    for email, acronym in zip(df['email'], df['acronym']):
+        source_blob_name = f'{acronym.lower()}.jpg'
+        target_blob_name = f'{email}.jpg'
+        print(f'Source Blob Name: {source_blob_name}, Target Blob Name: {target_blob_name}')
+
+        # Uncomment the following line if you're serious
+        # copy_blob(container_client, source_blob_name, target_blob_name)
+
+
+if __name__ == "__main__":
+    main();
